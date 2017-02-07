f5d78c50ff0292f73e3ee7a745b179478a58d8fd
Roberto Guido <bob4job@gmail.com>
2017-02-07 16:34:52 +0100

    feedback validazione form candidatura

--- resources/views/adoption/create.blade.php
+++ resources/views/adoption/create.blade.php
@@ -35,6 +35,16 @@
         </div>
 
         <div class="col-md-6">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             <form method="POST" action="{{ url('adozione') }}">
                 {!! csrf_field() !!}
                 <input type="text" name="name" placeholder="nome">
